cbea1638a2f50da2085d55037c4c07ecb5e92493
Karla Acosta <77418536+kacostam@users.noreply.github.com>
2025-02-06 03:42:10 +0000

    debugged label names for square root and square

--- app.rb
+++ app.rb
@@ -12,7 +12,7 @@ get("/square/new") do
 end
 
 get("/square/results") do
-  @the_num = params.fetch("users_number").to_f
+  @the_num = params.fetch("number").to_f
   @the_result = @the_num ** 2
   erb(:square_results)
 end
@@ -24,7 +24,7 @@ get("/square_root/new") do
 end
 
 get("/square_root/results") do
-  @the_num = params.fetch("users_number").to_f
+  @the_num = params.fetch("user_number").to_f
   @the_result = @the_num ** 0.5
   erb(:square_root_results)
 end
@@ -55,4 +55,8 @@ end
 
 get("/random/new") do
   erb(:random)
+end
+
+get("/random/results") do
+  erb(:random_results)
 end
