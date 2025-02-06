29cb0698284cc6bc74b6482c160e89582ff8f50c
Karla Acosta <77418536+kacostam@users.noreply.github.com>
2025-02-06 02:37:44 +0000

    draft v1: layouts and initial work on app.rb

--- app.rb
+++ app.rb
@@ -2,8 +2,25 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:hello)
+end
+
+get("/square/new") do
+  erb(:new_square_calc)
+end
+
+get("/square_root/new") do
+  erb(:square_root)
+end
+
+get("/payment/new") do
+  erb(:payment)
+end
+
+get("/random/new") do
+  erb(:random)
+end
+
+get("/square/results") do
+  erb(:square_results)
 end
